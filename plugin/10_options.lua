b40cc5fc1a0061b256e714f2acf4bcf970189a76
Joaquim Diaz <diazjoaquim@gmail.com>
2025-12-17 22:23:32 +0100

    [UPD] scrolloff + theme

--- plugin/10_options.lua
+++ plugin/10_options.lua
@@ -47,6 +47,7 @@ vim.o.splitkeep      = 'screen'   -- Reduce scroll during window split
 vim.o.splitright     = true       -- Vertical splits will be to the right
 vim.o.winborder      = 'single'   -- Use border in floating windows
 vim.o.wrap           = false      -- Don't visually wrap lines (toggle with \w)
+vim.o.scrolloff      = 10         -- Keep 10 lines visible
 
 vim.o.cursorlineopt  = 'screenline,number' -- Show cursor line per screen line
 
