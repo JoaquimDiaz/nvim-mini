70eff31722973a3bf8a476747cc51362bf508ee7
Joaquim Diaz <diazjoaquim@gmail.com>
2025-12-19 11:43:39 +0100

    [UPD] add func + key to clear rpl

--- after/plugin/iron.lua
+++ after/plugin/iron.lua
@@ -59,6 +59,7 @@ iron.setup {
 
 -- Iron Keymaps ===============================================================
 
+-- Mapping functions
 local nmap = function(lhs, rhs, desc)
   vim.keymap.set('n', lhs, rhs, { desc = desc })
 end
@@ -67,23 +68,29 @@ local nxmap = function(lhs, rhs, desc)
   vim.keymap.set({ 'n', 'x' }, lhs, rhs, { desc = desc })
 end
 
+-- Define a function to clear the REPL
+local clear_repl = function()
+  require('iron.core').send(nil, string.char(12))
+end
+
+-- Function to move the cursor after `send_code_block`
+local send_code_block_and_move = function()
+  iron.send_code_block(true)
+end
+
 -- REPL control
-nmap('<leader>rr', '<cmd>IronRepl<cr>',    'repl open')
-nmap('<leader>rR', '<cmd>IronRestart<cr>', 'repl restart')
-nmap('<leader>rf', '<cmd>IronFocus<cr>',   'repl focus')
-nmap('<leader>rh', '<cmd>IronHide<cr>',    'repl hide')
+nmap('<leader>rr', '<Cmd>IronRepl<CR>',    'repl open')
+nmap('<leader>rR', '<Cmd>IronRestart<CR>', 'repl restart')
+nmap('<leader>rf', '<Cmd>IronFocus<CR>',   'repl focus')
+nmap('<leader>rh', '<Cmd>IronHide<CR>',    'repl hide')
+nmap('<leader>rc', clear_repl,             'Clear REPL')
 
 -- Sending code
 nmap('<leader>rl',  iron.send_line,          'send line')
 nmap('<leader>rp',  iron.send_paragraph,     'send paragraph')
 nmap('<leader>ru',  iron.send_until_cursor,  'send until cursor')
 nmap('<leader>rF',  iron.send_file,          'send file')
-nxmap('<leader>rc', iron.send_motion,        'send chunk')
 
--- Function to move the cursor after `send_code_block`
-local send_code_block_and_move = function()
-  iron.send_code_block(true)
-end
 
 -- Blocks / marks
 nmap('<leader>rb', iron.send_code_block,            'send block')
